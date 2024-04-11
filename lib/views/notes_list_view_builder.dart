c8d6ca93993e8afe798408e7023136b80fb0669e
Peter-Awod <peterawod74@gmail.com>
2024-04-11 11:45:02 +0200

    update ui

--- lib/views/notes_list_view_builder.dart
+++ lib/views/notes_list_view_builder.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_masonry_view/flutter_masonry_view.dart';
 import 'package:notes_app/cubit/get_notes_cubit/get_notes_cubit.dart';
 import 'package:notes_app/cubit/get_notes_cubit/get_notes_states.dart';
 
@@ -11,21 +12,32 @@ class NotesListViewBuilder extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return BlocBuilder<GetNotesCubit,GetNoteStates>(
+    return BlocBuilder<GetNotesCubit, GetNoteStates>(
       builder: (context, state) {
+        List<NoteModel> notes =
+            BlocProvider.of<GetNotesCubit>(context).notes ?? [];
 
-        List<NoteModel>notes=BlocProvider.of<GetNotesCubit>(context).notes??[];
-
-        return ListView.builder(
-          itemCount: notes.length ,
-          physics: const BouncingScrollPhysics(),
-          scrollDirection: Axis.vertical,
-          itemBuilder: (context, index) {
-            return NoteCard(noteModel: notes[index],);
-          },
-
+        return SingleChildScrollView(
+          child: MasonryView(
+            listOfItem: notes,
+            numberOfColumn: 2,
+            itemBuilder: (item) {
+              return NoteCard(noteModel: item);
+            },
+            itemPadding: 0,
+          ),
         );
       },
     );
   }
 }
+
+// ListView.builder(
+// itemCount: notes.length ,
+// physics: const BouncingScrollPhysics(),
+// scrollDirection: Axis.vertical,
+// itemBuilder: (context, index) {
+// return NoteCard(noteModel: notes[index],);
+// },
+//
+// )
--- lib/widgets/Note/note_item_card.dart
+++ lib/widgets/Note/note_item_card.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:notes_app/cubit/get_notes_cubit/get_notes_cubit.dart';
+
 import 'package:notes_app/models/note_model.dart';
 import 'package:notes_app/views/edit_view.dart';
 
@@ -12,7 +11,7 @@ class NoteCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialButton(
-      onPressed: (){
+      onPressed: () {
         Navigator.push(
           context,
           MaterialPageRoute(
@@ -24,88 +23,91 @@ class NoteCard extends StatelessWidget {
       },
       padding: EdgeInsets.zero,
       child: Card(
-
         shape: OutlineInputBorder(borderRadius: BorderRadius.circular(12)),
         color: Color(noteModel.color),
-        child: Padding(
-          padding: const EdgeInsetsDirectional.only(
-            top: 24,
-            bottom: 24,
-            start: 24,
-          ),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.end,
-            children: [
-              ListTile(
-                title: Text(
-                  noteModel.title,
-                  maxLines: 1,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            ListTile(
+              title: Text(
+                noteModel.title,
+                maxLines: 1,
+                overflow: TextOverflow.ellipsis,
+                style: const TextStyle(
+                  fontSize: 20,
+                  color: Colors.black,
+                ),
+              ),
+              subtitle: Padding(
+                padding: const EdgeInsets.symmetric(vertical: 8.0),
+                child: Text(
+                  noteModel.subTitle,
+                  maxLines: 5,
                   overflow: TextOverflow.ellipsis,
-                  style: const TextStyle(
-                    fontSize: 28,
-                    color: Colors.black,
-                  ),
-                ),
-                subtitle: Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 16.0),
-                  child: Text(
-                    noteModel.subTitle,
-                    maxLines: 3,
-                    overflow: TextOverflow.ellipsis,
-                    style: TextStyle(
-                      fontSize: 18,
-                      color: Colors.black.withOpacity(0.5),
-                    ),
-                  ),
-                ),
-                trailing: IconButton(
-                  onPressed: () {
-                    noteModel.delete();
-                    BlocProvider.of<GetNotesCubit>(context).getAllNotes();
-                  },
-                  icon: Icon(
-                    Icons.delete,
-                    color: Colors.black.withOpacity(0.7),
-                    size: 32,
+                  style: TextStyle(
+                    fontSize: 16,
+                    color: Colors.black.withOpacity(0.5),
                   ),
                 ),
               ),
-              Padding(
-                padding: const EdgeInsetsDirectional.symmetric(
-                  horizontal: 16,
-                ),
-                child: Row(
-                  children: [
-                    Text(
-                      noteModel.date,
-                      style: TextStyle(
-                        fontSize: 12,
-                        color: Colors.black.withOpacity(0.7),
-                      ),
-                    ),
-                  const  Spacer(),
-                    IconButton(
-                      onPressed: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) => EditView(
-                              noteModel: noteModel,
-                            ),
-                          ),
-                        );
-                      },
-                      icon: Icon(
-                        Icons.edit_note_outlined,
-                        color: Colors.black.withOpacity(0.7),
-                        size: 32,
-                      ),
-                    )
-                  ],
+              // trailing: IconButton(
+              //   onPressed: () {
+              //     noteModel.delete();
+              //     BlocProvider.of<GetNotesCubit>(context).getAllNotes();
+              //   },
+              //   icon: Icon(
+              //     Icons.delete,
+              //     color: Colors.black.withOpacity(0.7),
+              //     size: 32,
+              //   ),
+              // ),
+            ),
+            Padding(
+              padding: const EdgeInsetsDirectional.only(
+                  start: 16, bottom: 16, end: 16),
+              child: Text(
+                noteModel.date,
+                style: TextStyle(
+                  fontSize: 12,
+                  color: Colors.black.withOpacity(0.7),
                 ),
               ),
-            ],
-          ),
+            ),
+            // Padding(
+            //   padding: const EdgeInsetsDirectional.symmetric(
+            //     horizontal: 16,
+            //   ),
+            //   child: Row(
+            //     children: [
+            //       Text(
+            //         noteModel.date,
+            //         style: TextStyle(
+            //           fontSize: 12,
+            //           color: Colors.black.withOpacity(0.7),
+            //         ),
+            //       ),
+            //       // const Spacer(),
+            //       // IconButton(
+            //       //   onPressed: () {
+            //       //     Navigator.push(
+            //       //       context,
+            //       //       MaterialPageRoute(
+            //       //         builder: (context) => EditView(
+            //       //           noteModel: noteModel,
+            //       //         ),
+            //       //       ),
+            //       //     );
+            //       //   },
+            //       //   icon: Icon(
+            //       //     Icons.edit_note_outlined,
+            //       //     color: Colors.black.withOpacity(0.7),
+            //       //     size: 32,
+            //       //   ),
+            //       // )
+            //     ],
+            //   ),
+            // ),
+          ],
         ),
       ),
     );
